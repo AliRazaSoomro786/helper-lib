332a81a2e3c8f7fa6974c2e3e1d5fcfc606af08c
Ali Raza Fareed <ali.fareed@intagleo.com>
2020-12-01 18:47:34 +0500

    Demo added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mixcat.helper.Helper;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Helper helper = new Helper(this);
+        helper.show();
     }
 }
